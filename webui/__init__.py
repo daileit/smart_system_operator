a97ce73bf3ac58ee25e456bfa49610fb5cdde0bd
Dai Le <dai.le@lifestyleproject.io>
2025-11-17 00:42:00 +0700

    chore(webui): rename files

--- webui/__init__.py
+++ webui/__init__.py
@@ -3,13 +3,13 @@ WebUI module for Smart System Operator.
 Contains all page definitions separated into individual modules.
 """
 
-from .login import login_page
-from .main import main_page
-from .dashboard import dashboard_page
-from .users import users_page
+from .login_page import login_page
+from .main_page import main_page
+from .dashboard_page import dashboard_page
+from .users_page import users_page
 
 __all__ = [
-    'login_page',
+    'login',
     'main_page',
     'dashboard_page',
     'users_page',
